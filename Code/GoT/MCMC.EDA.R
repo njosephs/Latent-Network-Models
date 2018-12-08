852cd889afd55ce0fc078b11531c0b504ffa3b54
njosephs <njosephs@bu.edu>
2018-12-08 17:42:14 -0500

    Stark family vs

--- Code/GoT/MCMC.EDA.R
+++ Code/GoT/MCMC.EDA.R
@@ -9,9 +9,10 @@ mcmc_array <- function (ns, nchains, params) {
                         parameters = params))
 }
 
-ns <- 10000
+ns <- 50000
+burn <- 20000
 Nv <- nrow(A)
-Nk <- 3
+Nk <- 4
 d <- 2
 # nchains <- 1
 # params <- c(paste0("mu", 1:Nk)
@@ -29,7 +30,7 @@ set.seed(589)
 res <- LNM.MCMC(A, Nk = Nk, d = d, ns = ns)
 
 ##### d = 1
-Z.map <- apply(res$Z[, 1, ], 1, mean)
+Z.map <- apply(res$Z[, 1, (burn+1):ns], 1, mean)
 hist(Z.map)
 plot(density(Z.map))
 plot(sort(Z.map))
@@ -37,17 +38,32 @@ clusters <- kmeans(Z.map, Nk)$cluster
 colnames(A)[clusters == 1]
 colnames(A)[clusters == 2]
 colnames(A)[clusters == 3]
+colnames(A)[clusters == 4]
+colnames(A)[clusters == 5]
 
 ##### d = 2
-plot(res$Z[4, 1, ], type = "l")
+# plot(res$Z[4, 1, ], type = "l")
 
-Z.map <- sapply(1:Nv, FUN = function(i) apply(res$Z[i, , ], 1, mean))
+Z.map <- sapply(1:Nv, FUN = function(i) apply(res$Z[i, , (burn+1):ns], 1, mean))
 plot(Z.map[1, ] ~ Z.map[2, ])
 
-clusters <- kmeans(t(Z.map), Nk)$cluster
+clusters <- kmeans(t(Z.map), 2)$cluster
 colnames(A)[clusters == 1]
 colnames(A)[clusters == 2]
 colnames(A)[clusters == 3]
+colnames(A)[clusters == 4]
 
-mu.map <- sapply(1:Nk, FUN = function(i) apply(res$mu[i, , ], 1, mean))
-sigma.map <- apply(res$sigma, 1, mean)
+# ns = 50000, burn = 20000, 2 clusters
+# Starks vs
+
+mu.map <- sapply(1:Nk, FUN = function(i) apply(res$mu[i, , (burn+1):ns], 1, mean))
+points(t(mu.map), pch = 3)
+
+##### Nk = 2
+K.probs <- apply(res$K[, ] - 1, 1, mean)
+plot(sort(K.probs))
+
+Z.conf <- ifelse(K.probs < .495, 1, ifelse(K.probs > .505, 2, "unsure"))
+colnames(A)[Z.conf == 1]
+colnames(A)[Z.conf == 2]
+colnames(A)[Z.conf == "unsure"]
